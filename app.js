709c032a9d407009034ede053a6c569f5449a5a7
almanbetovaa <a83382576@gmail.com>
2024-04-16 16:02:32 +0600

    lastcommit

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-//2
+
+
+
+var numbers = (number) => {
+    var sum = 0
+    for (num of number){
+        if (num > 0) {
+            sum+=num
+        }
+    }
+    return sum / number.length
+}
+
+console.log(numbers( [2, -78, 45,4,-7,-6,-10, 26, 30, 8]))
+
+
+
+
+
 var square = (numbers) => {
     var sum = 0
     for (number of numbers) {
@@ -15,9 +33,11 @@ console.log(square([1,2,3,4,5,6,7,8,9]))
 var findWords = (words) => {
     result = []
     for (let word of words) {
-        if (word.includes('t' && word.includes('a')))
+        if (word.toLowerCase().includes('t') || word.toLowerCase().includes('a'))
+
             result.push(word)
-        return result
+
     }
+    return result
 }
-console.log(findWords(['frontend', 'apple', 'text', 'course', 'test']))
+console.log(findWords(["Apple",'frontend', 'apple', 'text', 'course', 'test']))
