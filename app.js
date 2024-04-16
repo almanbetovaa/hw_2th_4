01d482de08994b3b420b1978bb3e1052d5681f1d
almanbetovaa <a83382576@gmail.com>
2024-04-16 15:24:03 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//2
+var square = (numbers) => {
+    var sum = 0
+    for (number of numbers) {
+        if (number % 2 === 0) {
+            sum += number * number
+        }
+    }
+    return sum
+}
+console.log(square([1,2,3,4,5,6,7,8,9]))
+
+
+//1
+var findWords = (words) => {
+    result = []
+    for ()
+}
